8dd08b075ebca492b268e12c85eaca76009b6c9b
tri-abadi <triabadi13@gmail.com>
2019-04-19 20:49:30 +0700

    DueTime

--- lib/AddTask.dart
+++ lib/AddTask.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class AddTask extends StatefulWidget {
   @override
@@ -6,10 +7,127 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
+  DateTime _dueDate = DateTime.now();
+
+  String _dateText = '';
+
+ 
+
+  Future<Null> _selectDueData(BuildContext context) async {
+    final picked = await showDatePicker(
+        context: context,
+        initialDate: _dueDate,
+        firstDate: DateTime(2019),
+        lastDate: DateTime(2080));
+
+    if (picked != null) {
+      setState(() {
+        _dueDate = picked;
+        _dateText = "${picked.day}/${picked.month}/${picked.year}";
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _dateText = "${_dueDate.day}/${_dueDate.month}/${_dueDate.year}";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Material(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("img/balloon.jpeg"), fit: BoxFit.cover),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("My Task ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      letterSpacing: 2.0,
+                    )),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Text("ADD TASK",
+                      style: TextStyle(fontSize: 24.0, color: Colors.white)),
+                ),
+                Icon(Icons.list, color: Colors.white, size: 30.0)
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  icon: Icon(Icons.dashboard),
+                  hintText: "New Task",
+                  border: InputBorder.none),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Icon(Icons.date_range),
+                ),
+                Expanded(
+                    child: Text("Due Date",
+                        style:
+                            TextStyle(fontSize: 22.0, color: Colors.black54))),
+                FlatButton(
+                    onPressed: () => _selectDueData(context),
+                    child: Text(_dateText,
+                        style:
+                            TextStyle(fontSize: 22.0, color: Colors.black54))),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  icon: Icon(Icons.note),
+                  hintText: "Note",
+                  border: InputBorder.none),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top:100.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.check,
+                    size: 48.0,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.close,
+                    size: 48.0,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
